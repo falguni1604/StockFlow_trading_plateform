3f31b8d392bf9f62a8b448eb01252ff53b989eea
falguni1604 <falgunikalbhut94@gmail.com>
2026-06-13 13:55:19 +0530

    Update API endpoint for fetching all positions

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -6,7 +6,7 @@ function Positions() {
     const [allPositions, setAllPositions] = useState([]);
 
     useEffect(() => {
-        axios.get("http://localhost:3002/allPositions").then((res) => {
+        axios.get("https://stockflow-trading-plateform-backend.onrender.com/allPositions").then((res) => {
             console.log("res.data");
             setAllPositions(res.data);
         });
